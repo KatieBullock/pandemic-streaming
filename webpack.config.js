67a4e497479986d8d9b20361fb908d0953ec66f0
KatieBullock <kathrynleahbullock@gmail.com>
2020-12-01 19:29:59 -0500

    set project up with webpack

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require("path");
+
+module.exports = {
+  entry: path.resolve(__dirname, "src/index.js"), // Your input file
+  output: {
+    filename: "main.js", // Your output filename
+    path: path.resolve(__dirname, "./public/assets"), // Output file path
+    publicPath: "/assets/", // Folder where all Webpack generated code will go
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/i,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+    ],
+  },
+  devServer: {
+    publicPath: "/assets/",
+    contentBase: path.resolve(__dirname, "./public"),
+    watchContentBase: true,
+  },
+};
